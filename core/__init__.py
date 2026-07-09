163f283ac64932231564c07249e0d24cfeaa6a87
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2026-07-09 18:07:24 +0530

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+from .workspace import Workspace, WorkspaceManager, MediaAsset, Operation
+from .asset_manager import AssetManager
+from .pipeline import FFmpegCompiler
+
+__all__ = ["Workspace", "WorkspaceManager", "MediaAsset", "Operation", "AssetManager", "FFmpegCompiler"]
