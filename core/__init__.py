f773c8770afa7b920dc5a0066b0738dbc6b88954
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2026-07-10 23:58:46 +0530

    Delete core directory

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .workspace import Workspace, WorkspaceManager, MediaAsset, Operation
-from .asset_manager import AssetManager
-from .pipeline import FFmpegCompiler
-
-__all__ = ["Workspace", "WorkspaceManager", "MediaAsset", "Operation", "AssetManager", "FFmpegCompiler"]
